bec5b3236cb00342d6de9ceb4e86d0d4897b6bbe
shashankpalingi <23071a6745@vnrvjiet.in>
2025-07-08 15:53:30 +0530

    Fix invalid STUN server URL and add multiple reliable STUN servers

--- frontend/src/components/VideoCall/VideoCallComponent.tsx
+++ frontend/src/components/VideoCall/VideoCallComponent.tsx
@@ -95,7 +95,11 @@ const VideoCallComponent: React.FC<VideoCallProps> = ({ roomId, onEndCall }) => 
           config: {
             iceServers: [
               { urls: 'stun:stun.l.google.com:19302' },
-              { urls: 'stun:global.stun.twilio.com:3478?transport=udp' }
+              { urls: 'stun:stun1.l.google.com:19302' },
+              { urls: 'stun:stun2.l.google.com:19302' },
+              { urls: 'stun:stun3.l.google.com:19302' },
+              { urls: 'stun:stun4.l.google.com:19302' },
+              { urls: 'stun:global.stun.twilio.com:3478' }
             ]
           }
         });
